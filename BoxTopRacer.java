48009b6a1be7d0a4bf1230ceee3aca78edbe4ac7
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro-6.local>
2014-12-08 15:02:51 +0000

    Lab08

--- BoxTopRacer.java
+++ BoxTopRacer.java
@@ -1,21 +1,40 @@
-/**
- * A BoxTopRacer is a Racer that can jump over hurdles of any width.
- * 
- * @author <...>
- * @version <...>
- *
- */
+ //A BoxTopRacer is a Racer that can jump over hurdles of any width.
+ //@author <Andre Purits>
+ 
+ import edu.fcps.karel2.Robot;
+   import edu.fcps.karel2.Display;
+   import javax.swing.JOptionPane;
+   
 public class BoxTopRacer extends Racer {
 	
 	//TODO Subclasses do not inherit any constructors from their superclass
 	//Consequently, you will need to provide a compatible constructor
+public BoxTopRacer (int y) 
+{
+   super(y);
+}
+
 	
-	/**
-	 * Re-implement the method to allow for jumping hurdles of any width
-	 */
+	 //Re-implement the method to allow for jumping hurdles of any width
+	 
 	@Override
 	public void jumpRight() {
-		
-	}
-
+	
+   turnLeft();
+   while(!rightIsClear()) {
+   move();
+   }
+   turnRight();
+   move();
+   while(!rightIsClear()) {
+   move();
+   }
+   turnRight();
+   move();
+   while(!rightIsClear()  && frontIsClear()) {
+   move();
+   }
+   turnLeft();
+	  
 }
+}   
--- SteepleChaseRacer.java
+++ SteepleChaseRacer.java
@@ -1,21 +1,39 @@
 /**
  * A SteepleChaseRacer is a Racer that can jump over hurdles of any height.
  * 
- * @author <...>
+ * @author <AndrePurits>
  * @version <...>
  *
  */
+ import edu.fcps.karel2.Robot;
+   import edu.fcps.karel2.Display;
+   import javax.swing.JOptionPane;
 public class SteepleChaseRacer extends Racer {
 	
 	//TODO Subclasses do not inherit any constructors from their superclass
 	//Consequently, you will need to provide a compatible constructor
-	
-	/**
-	 * Re-implement the method to allow for jumping hurdles of any height
-	 */
+	public SteepleChaseRacer (int y) 
+{
+   super(y);
+}
 	@Override
 	public void jumpRight() {
-		
-	}
-
+	
+   turnLeft();
+   while(!rightIsClear()) {
+   move();
+   }
+   turnRight();
+   move();
+   turnRight();
+   move();
+   while(!rightIsClear()  && frontIsClear()) {
+   move();
+   }
+   turnLeft();
+	  
 }
+}   
+   
+  
+
